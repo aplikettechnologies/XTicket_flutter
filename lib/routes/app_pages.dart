9b0261eeff2adcc912f33219d939f067ad769c5e
jayeshapliket <jayesh.kubavat@apliket.com>
2025-07-21 12:49:03 +0530

    setup assets folder and fonts

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,10 +1,8 @@
 import 'package:get/get.dart';
-
 import '../app/modules/onboarding/onboarding_screen.dart';
-import 'app_routes.dart';
 
 class AppPages {
   static final List<GetPage<dynamic>> routes = [
-    GetPage(name: AppRoutes.onboarding, page: () => OnboardingScreen()),
+    GetPage(name: "/", page: () => OnboardingScreen()),
   ];
 }
---
+++ lib/shared/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String baseSvg = "assets/svg/";
+  static const String baseFonts = "assets/fonts/";
+  static const String baseImages = "assets/images/";
+
+  // Onboarding
+  static const String bgOnboarding = "${baseSvg}bg_onboarding.svg";
+}
